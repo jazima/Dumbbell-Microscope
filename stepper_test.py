c85327fb27e1e63c4fd6614b4c57116ffcbd1bfc
Henry <hlewispm2@gmail.com>
2022-03-15 19:04:11 -0400

    fix stepper_test

--- stepper_test.py
+++ stepper_test.py
@@ -3,10 +3,16 @@ from time import sleep
 import api
 
 if __name__ == '__main__':
+    api.stepper_controller_init()
+
+    sleep(0.05)
+
     api.move_x_axis(5)
     api.move_y_axis(3)
     api.move_fine_focus(180)
 
+    sleep(0.05)
+    
     api.move_x_axis(-5)
     api.move_y_axis(-3)
     api.move_fine_focus(-180)
