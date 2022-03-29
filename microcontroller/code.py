949c46de60a19fd0afe03d4804ea94413bb993f0
Henry <hlewispm2@gmail.com>
2022-03-29 16:26:19 -0400

    Lint code.py

--- microcontroller/code.py
+++ microcontroller/code.py
@@ -22,17 +22,17 @@ import digitalio
 import usb_cdc
 
 # Global variables
-delay = 1 #delay between steps of the stepper motor in seconds
+delay = 1  # Delay between steps of the stepper motor in seconds
 
 #################################################################
 #                   Serial Communication Protocol               #
 #################################################################
-"""
+'''
 All signals are sent in bytes
 A single signal consists of 3 bytes, the first indicating the axis of movement
 the second indicating the direction of adjustment, and the third indicating the
 number of steps
-"""
+'''
 motor_axes = {
     "x": int(0).to_bytes(1, "big"),
     "y": int(1).to_bytes(1, "big"),
@@ -40,8 +40,8 @@ motor_axes = {
     "focus_coarse": int(3).to_bytes(1, "big")
 }
 
-#steps are given as an integer
-#direction is given as a bool
+# steps are given as an integer
+# direction is given as a bool
 #################################################################
 #                 End Serial Communication Protocol             #
 #################################################################
@@ -50,15 +50,15 @@ motor_axes = {
 uart = usb_cdc.data
 
 # Variable Definition of GPIO Pins
-x_dir_pin = board.GP15  ################## ENTER GPIO PIN ###################
-x_step_pin = board.GP14    ################## ENTER GPIO PIN ###################
-y_dir_pin = board.GP13   ################## ENTER GPIO PIN ###################
-y_step_pin = board.GP12   ################## ENTER GPIO PIN ###################
-fine_dir_pin = board.GP11 ################## ENTER GPIO PIN ###################
-fine_step_pin = board.GP10################## ENTER GPIO PIN ###################
-coarse_dir_pin = board.GP16 ################## ENTER GPIO PIN ###################
-coarse_step_pin = board.GP17 ################## ENTER GPIO PIN ###################
-enable_pin = board.GP9  ################## ENTER GPIO PIN ###################
+x_dir_pin = board.GP15
+x_step_pin = board.GP14
+y_dir_pin = board.GP13
+y_step_pin = board.GP12
+fine_dir_pin = board.GP11
+fine_step_pin = board.GP10
+coarse_dir_pin = board.GP16
+coarse_step_pin = board.GP17
+enable_pin = board.GP9
 
 # Set up input pins for stepper motor
 x_dir = digitalio.DigitalInOut(x_dir_pin)
@@ -88,38 +88,40 @@ coarse_step.direction = digitalio.Direction.OUTPUT
 enable = digitalio.DigitalInOut(enable_pin)
 enable.direction = digitalio.Direction.OUTPUT
 
-def step_motor(dir_pin: digitalio.DigitalInOut, step_pin:digitalio.DigitalInOut, steps:int, direction:bool) -> None:
-    """Moves the specified motor by the given number of steps in the given direction.
+
+def step_motor(dir_pin: digitalio.DigitalInOut, step_pin: digitalio.DigitalInOut, steps: int, direction: bool) -> None:
+    '''Moves the specified motor by the given number of steps in the given direction.
 
     :dir_pin: the pin corresponding to the direction of rotation of the specified motor.
     :step_pin: the pin corresponding to the step of the specified motor
     :steps: an integer number of steps to be taken by the specified motor
     :direction: boolean indicating the direction.
     :returns: None
-    """
+    '''
     dir_pin.value = direction
     enable.value = False
     for step in range(steps):
         step_pin.value = True
         time.sleep(delay)
-        step_pin_pin.value = False
+        step_pin.value = False
         time.sleep(delay)
     enable.value = True
     return
 
+
 def receive_signal():
-    """Receives and decodes the incoming serial signal
+    '''Receives and decodes the incoming serial signal
 
     :returns: tuple with the decoded signal
-    """
-    while uart.in_waiting < 3: #wait for serial input
+    '''
+    while uart.in_waiting < 3:  # Wait for serial input
         continue
 
     axis = uart.read(1)
     direction = int.from_bytes(uart.read(1), "big")
     steps = int.from_bytes(uart.read(1), "big")
 
-    #decode the axis
+    # Decode the axis
     if(axis == motor_axes["x"]):
         dir_pin = x_dir
         step_pin = x_step
@@ -134,6 +136,7 @@ def receive_signal():
         step_pin = coarse_step
 
     return(dir_pin, step_pin, direction, steps)
+
 
 enable.value = True
 while True:
